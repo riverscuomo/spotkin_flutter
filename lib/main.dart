e3bd3424bee030f7c01c298e982107152b43821a
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2024-07-12 05:20:22 -0700

    one config file

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,7 @@ void main() async {
 }
 
 Future<Map<String, dynamic>> loadConfig() async {
-  // load different config if in production mode
-  String configFileName = (isProduction || kReleaseMode ) ? 'assets/config_prod.json' : 'assets/config_dev.json';
-  String configString = await rootBundle.loadString(configFileName);
+  String configString = await rootBundle.loadString('assets/config.json');
   return jsonDecode(configString);
 }
 
