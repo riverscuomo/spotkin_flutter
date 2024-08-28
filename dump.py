d15b2424a1e2b772441f551b02d083a97a7a1b3f
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2024-08-28 09:42:35 -0700

    python env and dump

---
+++ dump.py
@@ -0,0 +1,4 @@
+from rivertils import dump
+
+dump.dump(['.gitignore',    '.git',  '.\dump.py',
+          '.\.vscode', '.dart_tool', 'build', 'doc', '.idea', 'assets', 'pubspec.lock', 'test', 'metadata'])
