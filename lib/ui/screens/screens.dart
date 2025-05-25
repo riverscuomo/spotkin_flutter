bcce6dd1b5b7345044eb7a2cb7ae87ba537b3076
Rivers <{ID}+{username}@users.noreply.github.com>
2025-05-25 13:48:56 -0400

    screens

--- lib/ui/screens/screens.dart
+++ lib/ui/screens/screens.dart
@@ -2,4 +2,3 @@ export 'package:spotkin_flutter/ui/screens/auth_screen.dart';
 export 'package:spotkin_flutter/ui/screens/home_screen.dart';
 export 'package:spotkin_flutter/ui/screens/setting_management_screen.dart';
 export 'package:spotkin_flutter/ui/widgets/bottom_sheets/search_bottom_sheet.dart';
-export 'package:spotkin_flutter/_archive/settings_screen.dart';
